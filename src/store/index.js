283ef217ebb7d77c36197a18d6a08558cb344910
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-13 15:54:43 +0000

    Update

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-   state: {
+  state: {
       albums: []
-    },
+  },
     mutations: {
       add (state, payload) {
         state.albums = payload
       }
-    }
-  })
+  },
+  actions: {},
+  getters: {}
+})

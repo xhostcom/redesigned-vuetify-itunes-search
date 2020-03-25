db41e54fe90174efdd432d491372b872b5fb025e
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-25 13:45:10 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,12 @@
     <v-layout mt-12 mb-12 justify-center align-center>
         <v-flex xs6 mt-12 mb-12>
         <v-text-field
-          v-model="search" autofocus
-          @keyup.enter="searchData($event)"
+          v-model="localValue"
+          autofocus
+          @keypress="fetchData"
           label="Enter Artists Name"
           append-icon="search"
-          ></v-text-field>
+        ></v-text-field>
         </v-flex>
       </v-layout>
        <h1>Card Contents</h1>
@@ -33,12 +34,28 @@
 import Footer from '@/components/Footer';
 export default {
   name: 'App',
+   props: {
+    value: String
+  },
   components: {
-
-    Footer
+  Footer
   },
   data: () => ({
-    //
+  localValue: "",
+  url_base: 'https://itunes.apple.com/search?entity=album&term=',
   }),
-};
+  created () {
+      this.localValue = this.value;
+      this.$watch("localValue", value => {
+      this.$emit("input", value);
+  });
+ },
+ methods: {
+        fetchData(e) {
+        if (e.key == "Enter") {
+        console.log(this.localValue);
+        }
+    }
+ }
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -4,17 +4,13 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
-import axios from "axios";
-Vue.store = Vue.prototype.store = 'store';
-Vue.prototype.$http = axios;
-Vue.prototype.axios = axios;
 
+Vue.store = Vue.prototype.store = 'store';
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   vuetify,
-  axios,
   render: h => h(App)
 }).$mount('#app')
